42ec11250cb8822dc979914b39d2794eb9cca996
Ryow <64392776+ryow1003@users.noreply.github.com>
2022-03-11 13:20:00 +0900

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -48,3 +48,13 @@ $(function(){
     return false;
   });
 });
+$(function(){
+  $('a[href^="#offer"]').click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
